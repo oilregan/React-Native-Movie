6f40f2e45f035ff2df033f93f82656217636bd81
oilregan <oil_regan@hotmail.com>
2017-05-25 17:32:03 +0700

    Edit

---
+++ MockData/MockData.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+export const MovieList = [
+    {
+        id: 1,
+        name_eng: "Pirates 5",
+        name_th: "สงครามแค้นโจรสลัดไร้ชีพ",
+        release_dates: "24/05/17",
+        imageURL: "https://res.cloudinary.com/dp3huct5o/image/upload/v1495587052/pirates5_ne8wgv.jpg",
+        coverURL: "https://res.cloudinary.com/dp3huct5o/image/upload/v1495618115/landscape-1488452253-pirates-of-the-caribbean-poster_lswqkv.jpg"
+    },
+    {
+        id: 2,
+        name_eng: "Alien Covenant",
+        name_th: "เอเลี่ยน โคเวแนนท์",
+        release_dates: "10/05/17",
+        imageURL: "https://res.cloudinary.com/dp3huct5o/image/upload/v1495587052/alien_g9jypg.jpg",
+        coverURL: "https://res.cloudinary.com/dp3huct5o/image/upload/v1495647943/alien-film-header-desktop-v2-front-main-stage_ewdfq1.jpg"
+    },
+    {
+        id: 3,
+        name_eng: "King Arthur",
+        name_th: "คิง อาร์เธอร์",
+        release_dates: "10/05/17",
+        imageURL: "https://res.cloudinary.com/dp3huct5o/image/upload/v1495587052/kin_jysknf.jpg",
+        coverURL: "https://res.cloudinary.com/dp3huct5o/image/upload/v1495647942/KingArthureLegendOfTheSword-1200x600_nprqef.jpg"
+    },
+    {
+        id: 4,
+        name_eng: "Guardians Galaxy 2",
+        name_th: "รวมพันธุ์นักสู้พิทักษ์จักรวาล2",
+        release_dates: "26/04/17",
+        imageURL: "https://res.cloudinary.com/dp3huct5o/image/upload/v1495587052/guardians_ayl462.jpg",
+        coverURL: "https://res.cloudinary.com/dp3huct5o/image/upload/v1495647942/cover_1737_ta2c6r.jpg"
+    },
+    {
+        id: 5,
+        name_eng: "Fast And Furious 8",
+        name_th: "เร็ว แรง ทะลุนรก 8",
+        release_dates: "12/04/17",
+        imageURL: "https://res.cloudinary.com/dp3huct5o/image/upload/v1495587052/fast8_lvxg1t.jpg",
+        coverURL: "https://res.cloudinary.com/dp3huct5o/image/upload/v1495647942/Fast-and-Furious-8_z1foez.jpg"
+    }
+
+];
---
+++ index.ios.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry
+} from 'react-native';
+
+import Main from './src/main';
+
+AppRegistry.registerComponent('movieList', () => Main);
